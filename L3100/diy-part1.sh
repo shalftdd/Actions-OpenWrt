d35604bc1defc42b1a0afa5b151e0a7ca2ae6f7b
shalftdd <83412163+shalftdd@users.noreply.github.com>
2021-12-02 11:39:08 +0800

    Create diy-part1.sh

---
+++ L3100/diy-part1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+svn co  https://github.com/moruiris/openwrt-packages/trunk/luci-app-chinadns-ng ./package/moruiris/luci-app-chinadns-ng
+svn co  https://github.com/moruiris/openwrt-packages/trunk/luci-app-adguardhome ./package/moruiris/luci-app-adguardhome
